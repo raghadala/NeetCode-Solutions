688e5aaa98a4c2364c933263ae601705ea06b57d
raghadala <135091725+raghadala@users.noreply.github.com>
2024-06-30 02:13:22 -0700

    Update 0015-3sum.py

--- 02_Two_Pointers/03_3Sum/0015-3sum.py
+++ 02_Two_Pointers/03_3Sum/0015-3sum.py
@@ -12,6 +12,28 @@ The space complexity is O(1) since we are not using any additional data structur
 """
 
 
+def threeSum(nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]: #if a is same as previous element, skip, avoid duplicates
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1 
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]: #skip duplicates
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1 # move pointers to look for next answer
+        return res
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
