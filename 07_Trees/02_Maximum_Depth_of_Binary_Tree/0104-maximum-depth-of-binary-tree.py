65dbdf0654d8dcdcb95a63906f0bb0fc7e5f41b8
raghadala <135091725+raghadala@users.noreply.github.com>
2024-07-04 00:23:21 -0700

    Update 0104-maximum-depth-of-binary-tree.py

--- 07_Trees/02_Maximum_Depth_of_Binary_Tree/0104-maximum-depth-of-binary-tree.py
+++ 07_Trees/02_Maximum_Depth_of_Binary_Tree/0104-maximum-depth-of-binary-tree.py
@@ -27,4 +27,4 @@ class Solution:
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
 
-        return max(left_depth, right_depth) + 1
+        return max(left_depth, right_depth) + 1  #add 1 to account for current node
