5f650622bfd6f7f3ee04d6f0dad3f184c94d5198
raghadala <135091725+raghadala@users.noreply.github.com>
2024-07-05 23:38:04 -0700

    Update 0056-merge-intervals.py

--- 16_Intervals/02_Merge_Intervals/0056-merge-intervals.py
+++ 16_Intervals/02_Merge_Intervals/0056-merge-intervals.py
@@ -16,14 +16,13 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
-
-        intervals.sort(key=lambda x: x[0])
-        result = [intervals[0]]
-
-        for interval in intervals[1:]:
+        
+        intervals.sort(key=lambda x:x[0]) #sort interval my first element (start time)
+        result = [intervals[0]] #start with first interval
+        for interval in intervals:
+            #if start time is <= to ending time of last interval
             if interval[0] <= result[-1][1]:
                 result[-1][1] = max(result[-1][1], interval[1])
             else:
                 result.append(interval)
-
         return result
