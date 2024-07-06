f9b93f24a62f92d649d99e9933261236eaf3676b
raghadala <135091725+raghadala@users.noreply.github.com>
2024-07-05 23:58:15 -0700

    Update 0252-meeting-rooms.py

--- 16_Intervals/04_Meeting_Rooms/0252-meeting-rooms.py
+++ 16_Intervals/04_Meeting_Rooms/0252-meeting-rooms.py
@@ -26,3 +26,14 @@ class Solution:
                 return False
 
         return True
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x:x[0])
+
+        for i in range(1,len(intervals)):
+            i1 = intervals[i]
+            i2 = intervals[i-1]
+            if i1.start < i2.end:
+                return False
+        return True
